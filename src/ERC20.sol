dae88a2bbb50a84ad6ba7e0261dd4aeea9405741
Killian Cassard <killian.cassard@epitech.eu>
2025-10-28 14:56:36 +0100

    step 2 completed

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./interface/IERC20.sol"
+import "./interface/IERC20.sol";
 
 contract ERC20 is IERC20 {
+    string public name = "kotter";
+    string public symbol = "KTTR";
+    uint8 public decimals = 5;
+
+    uint64 private _totalSupply;
+    mapping(address owner => uint64) private _balances;
+    mapping(address owner => mapping(address spender => uint64)) private _allowances;
+
+    constructor (uint64 totalSupply_) {
+        _totalSupply = totalSupply_;
+    }
 
 }
